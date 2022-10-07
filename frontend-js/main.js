381324af7390fb615c6b3a1b68147af2a9471ed2
Jung Woo <michaelwoo921@gmail.com>
2022-10-07 13:34:01 -0700

    showing chat interface

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,5 +1,11 @@
-import Search from "./modules/Search";
+import Search from "./modules/search";
+import Chat from "./modules/chat";
 
-if( document.querySelector('.header-search-icon')){
-    new Search()
+if(document.querySelector('#chat-wrapper')){
+    new Chat();
 }
+
+
+// if( document.querySelector('.header-search-icon')){
+//     new Search()
+// }
