b256d836ccbf3aba5e45175af8e88fc619f3d001
Jung Woo <michaelwoo921@gmail.com>
2022-10-07 16:43:01 -0700

    finishing chat feature, dynamic title

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,9 +1,12 @@
+import DOMPurify, { sanitize } from 'dompurify'
+
 class Chat {
     constructor(){
         this.openedYet = false;
         this.chatWrapper = document.querySelector('#chat-wrapper')
         this.openIcon = document.querySelector('.header-chat-icon')
         this.injectHTML()
+        this.chatLog = document.querySelector('#chat')
         this.chatField = document.querySelector('#chatField')
         this.chatForm = document.querySelector('#chatForm')
         this.closeIcon = document.querySelector('.chat-title-bar-close')
@@ -20,7 +23,19 @@ class Chat {
     }
     // methods
     sendMessageToServer(){
+        
         this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value})
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize( `
+        <div class="chat-self">
+            <div class="chat-message">
+            <div class="chat-message-inner">
+               ${this.chatField.value}
+            </div>
+            </div>
+            <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+        </div>
+        `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
         this.chatField.value=''
         this.chatField.focus()
     }
@@ -30,25 +45,40 @@ class Chat {
         }
         this.openedYet = true;
         this.chatWrapper.classList.add('chat--visible')
+        this.chatField.focus()
     }
     hideChat(){
         this.chatWrapper.classList.remove('chat--visible')
+
+    }
+    displayMessageFromServer(data){
+        console.log(data, this.chatLog)
+        this.chatLog.insertAdjacentHTML('beforeend', DOMPurify.sanitize(`
+        <div class="chat-other">
+            <a href="/profile/${data.username}"><img class="avatar-tiny" src="${data.avatar}"></a>
+            <div class="chat-message"><div class="chat-message-inner">
+            <a href="#"><strong>${data.username}:</strong></a>
+            ${data.message}
+            </div></div>
+        </div>
+        `))
+        this.chatLog.scrollTop = this.chatLog.scrollHeight
     }
 
     openConnection(){
         this.socket =io()
-        this.socket.on('chatMessageFromServer', function(data){
-            alert(data.message)
+        this.socket.on('welcome', data => {
+            this.username = data.username, this.avatar = data.avatar
+        })
+        this.socket.on('chatMessageFromServer', (data) => {
+           this.displayMessageFromServer(data)
         })
 
 
     }
     injectHTML(){
         this.chatWrapper.innerHTML=`<div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
-        <div id="chat" class="chat-log">
-      
-    
-        </div>
+        <div id="chat" class="chat-log"></div>
         
         <form id="chatForm" class="chat-form border-top">
           <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
